7a664038e9b20c6b7bfb447a8d693de088b6866d
zhangguoqi <zhangguo7@aliyun.com>
2017-06-20 10:14:39 +0800

    tool_funcs添加other2str函数

--- tools/tool_funcs.py
+++ tools/tool_funcs.py
@@ -10,3 +10,15 @@ def other2int(other):
         return int(other)
     except:
         return float('nan')
+		
+def other2str(other):
+    """其他类型转int类型
+
+    :param other: 其他类型转int类型，不能转换则指定一个默认值
+    :param except_value: 不能转换返回的默认值
+    :return: int类型的value
+    """
+    try:
+        return str(other)
+    except:
+        return ''
